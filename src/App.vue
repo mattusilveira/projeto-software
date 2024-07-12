0ced8a7f17762ceeb96b0d5ea85e8e5fe259ab6f
mattusilveira <matheus12062007silveira7@gmail.com>
2024-07-12 09:48:06 -0300

    primeiro commit, Criação das divs e css

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    <div class="Home">
+        <h1>Manutenção</h1>
+    </div>
+</template>
+
+<style scoped>
+div.Home {
+    background-color: rgb(23, 55, 141);
+    padding: 100px 10px;
+}
+</style>
